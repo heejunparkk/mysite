8dd9cb5ae9fb851a101df691c77dc2f8a4abba38
heejun <gmlwns2002@gmail.com>
2022-03-15 18:25:55 +0900

    mysite-start

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import { Search } from "@material-ui/icons";
+import { Search, ShoppingBasketOutlined } from "@material-ui/icons";
 import React from "react";
 import "./Header.css";
 
@@ -7,24 +7,34 @@ function Header() {
     <div className="header">
       <img
         className="header_logo"
+        alt="Logo"
         src={process.env.PUBLIC_URL + `/img/png01.png`}
       />
+
       <div className="header_search">
         <input className="header_searchInput" type="text" />
         <Search className="header_searchIcon" />
       </div>
+
       <div className="header_nav">
         <div className="header_option">
           <span className="header_optionLineOne"> 안녕하세요!!</span>
           <span className="header_optionLineTwo"> Login</span>
         </div>
+
         <div className="header_option">
           <span className="header_optionLineOne"> 돌아가기</span>
           <span className="header_optionLineTwo"> 주문내역</span>
         </div>
+
         <div className="header_option">
           <span className="header_optionLineOne"> 반가워요</span>
           <span className="header_optionLineTwo"> 좋아요</span>
+        </div>
+
+        <div className="header_optionBasket">
+          <ShoppingBasketOutlined />
+          <span className="header_optionLineTwo_basket"> 장바구니</span>
         </div>
       </div>
     </div>
