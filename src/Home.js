67ab960c1a48bf292a3f3845f6542fd5a8fd6a41
heejun <gmlwns2002@gmail.com>
2022-03-18 02:08:16 +0900

    mysite product

--- src/Home.js
+++ src/Home.js
@@ -12,14 +12,52 @@ function Home() {
           alt=""
         />
         <div className="home_row">
-          <Product />
+          <Product
+            id="1"
+            title="제품1"
+            price={"10,000"}
+            image="/img/doodle mat - image 01 - 3.jpg"
+            rating={5}
+          />
+          <Product
+            id="2"
+            title="제품2"
+            price={"10,000"}
+            image="/img/doodle mat - image 05 - 2.jpg"
+            rating={2}
+          />
         </div>
         <div className="home_row">
-          <Product />
-          <Product />
+          <Product
+            id="3"
+            title="제품3"
+            price={"10,000"}
+            image="/img/doodle mat - image 06 - 2.jpg"
+            rating={3}
+          />
+          <Product
+            id="4"
+            title="제품4"
+            price={"10,000"}
+            image="/img/doodle mat - image 07 - 2.jpg"
+            rating={4}
+          />
+          <Product
+            id="5"
+            title="제품5"
+            price={"10,000"}
+            image="/img/doodle mat - image 06 - 2.jpg"
+            rating={5}
+          />
         </div>
         <div className="home_row">
-          <Product />
+          <Product
+            id="6"
+            title="제품6"
+            price={"10,000"}
+            image="/img/doodle mat - image 05 - 2.jpg"
+            rating={2}
+          />
         </div>
       </div>
     </div>
