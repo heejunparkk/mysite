f8405833633bcaea8d78a70c5e900b040018451e
heejun <gmlwns2002@gmail.com>
2022-03-29 00:47:15 +0900

    cart item remove

--- src/Reducer.js
+++ src/Reducer.js
@@ -13,6 +13,19 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn("(id: " + action.id + ")이 장바구니에 존재하지 않습니다.");
+      }
+      return { ...state, basket: newBasket };
 
     default:
       return state;
