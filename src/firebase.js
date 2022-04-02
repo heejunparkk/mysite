c53b902ebb8d0a6e02177610c3d86a3d0bc34541
heejun <gmlwns2002@gmail.com>
2022-04-03 00:01:42 +0900

    mysite loginpage

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQU_DFfHYURWM3xw7Y7BQxvZjfWdmHrVA",
+  authDomain: "mysite-2ee22.firebaseapp.com",
+  projectId: "mysite-2ee22",
+  storageBucket: "mysite-2ee22.appspot.com",
+  messagingSenderId: "456167997814",
+  appId: "1:456167997814:web:a9a4e3823880d762ff3a57",
+  measurementId: "G-KEV2BXYLBM",
+};
